86a46730ae91fe0d0142c6247cd118fe47af87e3
adirizka7 <adiunity@gmail.com>
2018-12-26 07:07:41 +0000

    add missed character

--- script/text_preprocess.py
+++ script/text_preprocess.py
@@ -1,10 +1,10 @@
 import wikipedia
 import re
-p = wikipedia.page("joko widodo")
+p = wikipedia.page("christian bautista")
 content = p.content.split('References')[0]
 content = content.split('See also')[0]
 content = re.sub(r'[=]*[a-zA-Z ü]*[=]+', '', content)
 content = re.sub(r'[\n]+', ' ', content)
-content = re.sub(r'[:<>+=_`~!@#$%^&*;,\'()/.\"0-9-]+', ' ', content)
+content = re.sub(r'[–:<>+=_`~!@#$%^&*;,\'()/.\"0-9-]+', ' ', content)
 content = re.sub(r'[ ]+', ' ', content)
 print(content)
