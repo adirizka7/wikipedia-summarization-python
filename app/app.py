233b6a9888c22b800c05752948d074270c9eacd1
adirizka7 <adiunity@gmail.com>
2018-12-26 06:55:11 +0000

    worked flask post and added more preprocessing

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("error.html")
+def main():
+    return render_template("Home.html")
+
+@app.route("/handle_data", methods=['POST'])
+def handle_data():
+    result = request.form.to_dict()
+    return (str(result))
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = '5001', debug = True)
--- script/text_preprocess.py
+++ script/text_preprocess.py
@@ -5,4 +5,6 @@ content = p.content.split('References')[0]
 content = content.split('See also')[0]
 content = re.sub(r'[=]*[a-zA-Z ü]*[=]+', '', content)
 content = re.sub(r'[\n]+', ' ', content)
+content = re.sub(r'[:<>+=_`~!@#$%^&*;,\'()/.\"0-9-]+', ' ', content)
+content = re.sub(r'[ ]+', ' ', content)
 print(content)
